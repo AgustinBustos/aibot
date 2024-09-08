fcbcebe94c1924f04adac3e4ccd5c12a3924967b
Agustin Bustos Barton <agustinbustosbarton@gmail.com>
2024-09-08 20:53:28 -0300

    full html

--- reducehtml.py
+++ reducehtml.py
@@ -1,13 +1,36 @@
 from bs4 import BeautifulSoup
+import bs4
+from bs4 import Comment
 
 def html_remover(html,full=False):
     soup = BeautifulSoup(html, 'html.parser')
     if full:
-        for tag in soup.find_all(['meta', 'style']):
-            tag.decompose()
-
+        # for tag in soup.find_all(['meta', 'style']):
+        #     tag.decompose()
         # for tag in soup.find_all(True):  # True means all tags
         #     tag.attrs = {}
+        
+        # tag_list = soup.findAll(lambda tag: len(tag.attrs) > 0)
+        # for t in tag_list:
+        #     for attr, val in t.attrs:
+        #         del t[attr]
+        # return soup
+        KEEP_ATTRIBUTES = ['href']
+
+        # doc = '''<html><head><title>Page title</title></head><body><p id="firstpara" align="center">This is <i>paragraph</i> <a onmouseout="">one</a>.<p id="secondpara" align="blah">This is <i>paragraph</i> <b>two</b>.</html>'''
+        # soup = bs4.BeautifulSoup(doc)
+        
+
+        for child in soup.descendants:
+            if isinstance(child,Comment):
+                child.extract()
+        for tag in soup.descendants:
+            if isinstance(tag, bs4.element.Tag):
+                tag.attrs = {key: value for key, value in tag.attrs.items()
+                            if key in KEEP_ATTRIBUTES}
+        for div in soup.find_all('div'):
+            if not div.attrs:
+                div.unwrap()
 
     cleaned_html = soup.prettify()
     return cleaned_html
--- supai.py
+++ supai.py
@@ -542,12 +542,18 @@ if __name__ == '__main__':
 
 
     driver.get(r"https://www.linkedin.com/jobs/search/?currentJobId=3847620136&distance=25&f_TPR=r86400&geoId=100446943&keywords=Cient%C3%ADfico%20de%20datos&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true")
-    page_source = driver.page_source
-    # print(page_source)
-    # print(len(page_source))
-    # print(len(html_remover(page_source)))
-    list_of_jobs=driver.find_element(By.CSS_SELECTOR, name_of_class_for_list_of_jobs).get_attribute('outerHTML')
-    list_of_jobs=html_remover(list_of_jobs)
-    print(get_job_links(list_of_jobs))
+    html = driver.page_source
+
+    # list_of_jobs=driver.find_element(By.CSS_SELECTOR, name_of_class_for_list_of_jobs).get_attribute('outerHTML')
+    # list_of_jobs=html_remover(list_of_jobs)
+    # print(get_job_links(list_of_jobs))
+
+    # keep only if it has href
+    simplified_tml=html_remover(html,full=True)
+    print(simplified_tml)
+    print(len(html))
+    print(len(simplified_tml))
+
+
  
     time.sleep(10000)
